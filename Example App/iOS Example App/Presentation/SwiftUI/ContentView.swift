9b02c94a9c43691c1b62e3b1e35f125710eb64d3
Ivan Izyumkin <izzyumkin@gmail.com>
2023-05-25 22:28:48 +0300

    Some fixes in SwiftUI preview

--- Example App/iOS Example App/Presentation/SwiftUI/ContentView.swift
+++ Example App/iOS Example App/Presentation/SwiftUI/ContentView.swift
@@ -1,9 +1,24 @@
+// MIT License
 //
-//  ContentView.swift
-//  iOS Example App
+// Copyright (c) 2023 Ivan Izyumkin
 //
-//  Created by Иван Изюмкин on 24.05.2023.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 import SwiftUI
 import SnapCollection
@@ -12,24 +27,24 @@ import SnapCollection
 struct ContentView: View {
     @State var items = [Int](0...100)
     
-    private func shuffle() {
-        items.shuffle()
-    }
-    
     var body: some View {
-        VStack {
-            SnapCollection(items: items, itemSize: CGSize(width: 50, height: 50), scrollDirection: .vertical, cell: { indexPath, item in
-                GeometryReader { geometry in
-                    Button(action: shuffle) {
-                        Text(String(item))
-                            .foregroundColor(.black)
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                            .background(Color.blue)
-                    }
+        SnapCollection(
+            items: items,
+            itemSize: CGSize(width: 50, height: 50),
+            scrollDirection: .vertical,
+            cell: { indexPath, item in
+                Button(action: shuffle) {
+                    Text(String(item))
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .foregroundColor(.white)
+                        .background(Color.blue)
                 }
             })
-            .frame(maxWidth: 50, maxHeight: .infinity)
-        }
+        .frame(maxWidth: 50, maxHeight: .infinity)
+    }
+    
+    private func shuffle() {
+        items.shuffle()
     }
 }
 
